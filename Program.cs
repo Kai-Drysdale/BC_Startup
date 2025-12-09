309f64dc4ec039be0a9da970bc8b2cb27955cd7c
kai-drysdale <kai.drysdale4@protonmail.com>
2025-12-09 12:16:16 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -24,10 +24,7 @@ namespace BC_Startup
         static void Main()
         {
             //check if another instance of BC_Startup is already running
-           if (!mutex.WaitOne(0, true))
-{
-    Environment.Exit(0);
-}
+            if (System.Diagnostics.Process.GetProcessesByName(System.IO.Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetEntryAssembly().Location)).Count() > 1) System.Diagnostics.Process.GetCurrentProcess().Kill();
 
             //if nav serviec is running then launch straight away
             if (IsServiceRunning(navServiceName))
